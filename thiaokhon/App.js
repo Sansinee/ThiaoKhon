9d048d005a55ab43c2b9dab413cab59a6378b3e5
zsarinya10z <zpaatz10@gmail.com>
2020-08-18 22:21:21 +0700

    add login

--- thiaokhon/App.js
+++ thiaokhon/App.js
@@ -5,45 +5,60 @@
  * @format
  * @flow strict-local
  */
-import React, { Component } from 'react';
-import { Alert, AppRegistry, StyleSheet, Text, TouchableHighlight, TextInput, View } from 'react-native';
+import React, {Component} from 'react';
+import {
+  Alert,
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  View,
+  ScrollView,
+} from 'react-native';
 
 export default class Touchables extends Component {
   state = {
     email: '',
-    password: ''
-  }
+    password: '',
+  };
   _onPressButton(email, pass) {
-    alert('email: ' + email + ' password: ' + pass)
+    alert('email: ' + email + ' password: ' + pass);
   }
 
   _onLongPressButton() {
-    Alert.alert('You long-pressed the button!')
+    Alert.alert('You long-pressed the button!');
   }
-
-
 
   render() {
     return (
       <View style={styles.container}>
-        <TextInput style = {styles.input}
-               underlineColorAndroid = "transparent"
-               placeholder = "Email"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               onChangeText = {(text)=>this.setState({ email: text })}/>            
-        <TextInput style = {styles.input}
-               underlineColorAndroid = "transparent"
-               placeholder = "Password"
-               placeholderTextColor = "#9a73ef"
-               autoCapitalize = "none"
-               onChangeText = {(text)=>this.setState({ password: text })}/>
-        <TouchableHighlight 
-          onPress={()=>this._onPressButton(this.state.email, this.state.password)} 
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Email"
+          placeholderTextColor="#111111"
+          autoCapitalize="none"
+          onChangeText={(text) => this.setState({email: text})}
+        />
+        <TextInput
+          style={styles.input}
+          underlineColorAndroid="transparent"
+          placeholder="Password"
+          placeholderTextColor="#111111"
+          autoCapitalize="none"
+          onChangeText={(text) => this.setState({password: text})}
+        />
+        <TouchableHighlight
+          onPress={() =>
+            this._onPressButton(this.state.email, this.state.password)
+          }
           onLongPress={this._onLongPressButton}
           underlayColor="white">
           <View style={styles.button}>
-            <Text style={styles.buttonText}>Touchable with Long Press</Text>
+            <Text style font={styles.buttonText}>
+              Login
+            </Text>
           </View>
         </TouchableHighlight>
       </View>
@@ -54,21 +69,24 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 300,
     alignItems: 'center',
-    backgroundColor: '#0D47A1',
+    backgroundColor: 'white',
   },
   //text button*
   button: {
+    borderRadius: 13,
     marginBottom: 30,
     width: 260,
+    height: 30,
     alignItems: 'center',
     backgroundColor: '#80e2ff',
   },
   //text input
   input: {
+    borderRadius: 13,
     margin: 15,
     height: 40,
     width: 260,
-    borderColor: '#7a42f4',
+    borderColor: '#111111',
     borderWidth: 1,
     backgroundColor: 'white',
   },
