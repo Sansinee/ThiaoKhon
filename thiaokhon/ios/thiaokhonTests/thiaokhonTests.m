5ab410798562b059c9aa409841bbe0bbaf378537
Sansinee Rakniem <47499591+Sansinee@users.noreply.github.com>
2020-08-18 18:03:41 +0700

    add app

--- thiaokhon/ios/thiaokhonTests/thiaokhonTests.m
+++ thiaokhon/ios/thiaokhonTests/thiaokhonTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ProjectTests : XCTestCase
+@interface thiaokhonTests : XCTestCase
 
 @end
 
-@implementation ProjectTests
+@implementation thiaokhonTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
