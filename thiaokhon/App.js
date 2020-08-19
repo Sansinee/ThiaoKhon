fe97e4cb649a1248eb6d3eaabbce4c8487d1e4a0
zsarinya10z <zpaatz10@gmail.com>
2020-08-19 12:04:30 +0700

    add login

--- thiaokhon/App.js
+++ thiaokhon/App.js
@@ -1,93 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React, {Component} from 'react';
-import {
-  Alert,
-  AppRegistry,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  TextInput,
-  View,
-  ScrollView,
-} from 'react-native';
+/* This is an Login Registration example from https://aboutreact.com/ */
+/* https://aboutreact.com/react-native-login-and-signup/ */
 
-export default class Touchables extends Component {
-  state = {
-    email: '',
-    password: '',
-  };
-  _onPressButton(email, pass) {
-    alert('email: ' + email + ' password: ' + pass);
-  }
+//Import React
+import React from 'react';
 
-  _onLongPressButton() {
-    Alert.alert('You long-pressed the button!');
-  }
+//Import Navigators from React Navigation
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="     Email"
-          placeholderTextColor="#111111"
-          autoCapitalize="none"
-          onChangeText={(text) => this.setState({email: text})}
-        />
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="     Password"
-          placeholderTextColor="#111111"
-          autoCapitalize="none"
-          onChangeText={(text) => this.setState({password: text})}
-        />
-        <TouchableHighlight
-          onPress={() =>
-            this._onPressButton(this.state.email, this.state.password)
-          }
-          onLongPress={this._onLongPressButton}
-          underlayColor="white">
-          <View style={styles.button}>
-            <Text style font={styles.buttonText}>
-              Login
-            </Text>
-          </View>
-        </TouchableHighlight>
-      </View>
-    );
-  }
-}
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 300,
-    alignItems: 'center',
-    backgroundColor: 'white',
+//Import all the screens needed
+import SplashScreen from './Screen/SplashScreen';
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
+
+const Auth = createStackNavigator({
+  //Stack Navigator for Login and Sign up Screen
+  LoginScreen: {
+    screen: LoginScreen,
+    navigationOptions: {
+      headerShown: false,
+    },
   },
-  //text button*
-  button: {
-    borderRadius: 13,
-    marginBottom: 30,
-    width: 260,
-    height: 30,
-    alignItems: 'center',
-    backgroundColor: '#01579B',
-  },
-  //text input
-  input: {
-    borderRadius: 13,
-    margin: 15,
-    height: 40,
-    width: 260,
-    borderColor: '#111111',
-    borderWidth: 1,
-    backgroundColor: 'white',
+  RegisterScreen: {
+    screen: RegisterScreen,
+    navigationOptions: {
+      title: 'Register',
+      headerStyle: {
+        backgroundColor: '#307ecc',
+      },
+      headerTintColor: '#fff',
+    },
   },
 });
+
+/* Switch Navigator for those screens which needs to be switched only once
+  and we don't want to switch back once we switch from them to the next one */
+const App = createSwitchNavigator({
+  SplashScreen: {
+    /* SplashScreen which will come once for 5 Seconds */
+    screen: SplashScreen,
+    navigationOptions: {
+      /* Hiding header for Splash Screen */
+      headerShown: false,
+    },
+  },
+  Auth: {
+    /* Auth Navigator which includer Login Signup will come once */
+    screen: Auth,
+  },
+  DrawerNavigationRoutes: {
+    /* Navigation Drawer as a landing page */
+    screen: DrawerNavigationRoutes,
+    navigationOptions: {
+      /* Hiding header for Navigation Drawer as we will use our custom header */
+      headerShown: false,
+    },
+  },
+});
+
+export default createAppContainer(App);
