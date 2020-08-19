b73fa290354572a0035db3980538c8f74491a502
zsarinya10z <zpaatz10@gmail.com>
2020-08-20 01:13:59 +0700

    add

--- thiaokhon/screen/drawerScreens/HomeScreen.js
+++ thiaokhon/screen/drawerScreens/HomeScreen.js
@@ -2,9 +2,9 @@
 /* https://aboutreact.com/react-native-login-and-signup/ */
 
 //Import React
-import React from 'react'
-import { Text, View } from 'react-native'
-import Swiper from 'react-native-swiper'
+import React from 'react';
+import {Text, View} from 'react-native';
+import Swiper from 'react-native-swiper';
 
 //style
 var styles = {
@@ -13,52 +13,49 @@ var styles = {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#9DD6EB'
+    backgroundColor: '#9DD6EB',
   },
   slide2: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#97CAE5'
+    backgroundColor: '#97CAE5',
   },
   slide3: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#92BBD9'
+    backgroundColor: '#92BBD9',
   },
   text: {
     color: '#fff',
     fontSize: 30,
-    fontWeight: 'bold'
-  }
-}
-
+    fontWeight: 'bold',
+  },
+};
 
 const HomeScreen = () => {
   global.currentScreenIndex = 'HomeScreen';
   return (
-    <View style={{flex: 1, alignItems: 'center', marginTop: 100}}> 
-    {/* add img for slide */}
-    <Swiper style={styles.wrapper} showsButtons loop={false}>
-    <View testID="Hello" style={styles.slide1}>
-      <Text style={styles.text}>Hello Swiper</Text>
-    </View>
-    <View testID="Beautiful" style={styles.slide2}>
-      <Text style={styles.text}>Beautiful</Text>
-    </View>
-    <View testID="Simple" style={styles.slide3}>
-      <Text style={styles.text}>And simple</Text>
-    </View>
-  </Swiper>
+    <View style={{flex: 1, alignItems: 'center', marginTop: 100}}>
+      {/* add img for slide */}
+      <Swiper style={styles.wrapper} showsButtons loop={false}>
+        <View testID="Hello" style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+        </View>
+        <View testID="Beautiful" style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View testID="Simple" style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
       <Text style={{fontSize: 23, marginTop: 10}}>Home Screen</Text>
       <Text style={{fontSize: 18, marginTop: 10}}>
         Simple Login Registraction Example
       </Text>
       <Text style={{fontSize: 18, marginTop: 10}}>https://aboutreact</Text>
-     
     </View>
-    
   );
 };
 export default HomeScreen;
