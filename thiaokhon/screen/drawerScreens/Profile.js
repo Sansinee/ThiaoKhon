f22d452cd281153a19a06d089f124e4357422111
zsarinya10z <zpaatz10@gmail.com>
2020-10-10 18:16:54 +0700

    add

--- thiaokhon/screen/drawerScreens/Profile.js
+++ thiaokhon/screen/drawerScreens/Profile.js
@@ -1,68 +1,31 @@
-// import React from 'react';
-// import {StyleSheet, Text, View, Image, ScrollView} from 'react-native';
-
-// const FlatListBasics = () => {
-//   return (
-//     <View>
-//       <View style={styles.container}>
-//         <Text style={styles.title}>Name</Text>
-//       </View>
-//       <View style={styles.container}>
-//         <Text style={styles.title}>Mail</Text>
-//       </View>
-//   </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     margin: 20,
-//     backgroundColor: "#eaeaea"
-//   },
-//   title: {
-//     // borderWidth: 4,
-//     // borderColor: "#20232a",
-//     borderRadius: 6,
-//     backgroundColor: "#61dafb",
-//     color: "#20232a",
-//     // textAlign: "center",
-//     fontSize: 20,
-//     // fontWeight: "bold"
-//   }
-// });
-
-// export default FlatListBasics;
-
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity
-} from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 
 export default class Profile extends Component {
-
   render() {
     return (
       <View style={styles.container}>
-          <View style={styles.header}></View>
-          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
-          <View style={styles.body}>
-            <View style={styles.bodyContent}>
-              <Text style={styles.name}>John Doe</Text>
-              <Text style={styles.info}>UX Designer / Mobile developer</Text>
-              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
-              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 1</Text>  
-              </TouchableOpacity>              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 2</Text> 
-              </TouchableOpacity>
-            </View>
+        <View style={styles.header}></View>
+        <Image
+          style={styles.avatar}
+          source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}
+        />
+        <View style={styles.body}>
+          <View style={styles.bodyContent}>
+            <Text style={styles.name}>John Doe</Text>
+            <Text style={styles.info}>UX Designer / Mobile developer</Text>
+            <Text style={styles.description}>
+              Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum
+              electram expetendis, omittam deseruisse consequuntur ius an,
+            </Text>
+
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Opcion 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>Opcion 2</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     );
@@ -70,61 +33,59 @@ export default class Profile extends Component {
 }
 
 const styles = StyleSheet.create({
-  header:{
-    backgroundColor: "#00BFFF",
-    height:200,
+  header: {
+    backgroundColor: '#00BFFF',
+    height: 200,
   },
   avatar: {
     width: 130,
     height: 130,
     borderRadius: 63,
     borderWidth: 4,
-    borderColor: "white",
-    marginBottom:10,
-    alignSelf:'center',
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
     position: 'absolute',
-    marginTop:130
+    marginTop: 130,
   },
-  name:{
-    fontSize:22,
-    color:"#FFFFFF",
-    fontWeight:'600',
+  name: {
+    fontSize: 22,
+    color: '#FFFFFF',
+    fontWeight: '600',
   },
-  body:{
-    marginTop:40,
+  body: {
+    marginTop: 40,
   },
   bodyContent: {
     flex: 1,
     alignItems: 'center',
-    padding:30,
+    padding: 30,
   },
-  name:{
-    fontSize:28,
-    color: "#696969",
-    fontWeight: "600"
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: '600',
   },
-  info:{
-    fontSize:16,
-    color: "#00BFFF",
-    marginTop:10
+  info: {
+    fontSize: 16,
+    color: '#00BFFF',
+    marginTop: 10,
   },
-  description:{
-    fontSize:16,
-    color: "#696969",
-    marginTop:10,
-    textAlign: 'center'
+  description: {
+    fontSize: 16,
+    color: '#696969',
+    marginTop: 10,
+    textAlign: 'center',
   },
   buttonContainer: {
-    marginTop:10,
-    height:45,
+    marginTop: 10,
+    height: 45,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
-    backgroundColor: "#00BFFF",
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#00BFFF',
   },
 });
-
-                                            
