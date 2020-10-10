f9c0603731b1d03ac753d88c6c75322e98d49130
Sansinee Rakniem <47499591+Sansinee@users.noreply.github.com>
2020-10-10 17:34:17 +0700

    edit pro

--- thiaokhon/screen/drawerScreens/HomeScreen.js
+++ thiaokhon/screen/drawerScreens/HomeScreen.js
@@ -68,12 +68,3 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
   },
 });
-
- /* <View style={{flex: 1, flexDirection: 'row'}}>
-      <View
-        style={{width: 50, height: 50, backgroundColor: 'powderblue'}}
-      />
-      <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-              
-      <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-    </View> */
