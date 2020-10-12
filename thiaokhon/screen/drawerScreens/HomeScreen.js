04c830937a898d2ad01fb7b2c914d4bb94f55bcf
Sansinee Rakniem <47499591+Sansinee@users.noreply.github.com>
2020-10-12 18:35:05 +0700

    add

--- thiaokhon/screen/drawerScreens/HomeScreen.js
+++ thiaokhon/screen/drawerScreens/HomeScreen.js
@@ -1,23 +1,28 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text,SafeAreaView, ScrollView, View, Image} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {SliderBox} from 'react-native-image-slider-box';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //เก็บรูปไว้ใน state สามารถเก็บได้ทั้ง url หรือ folder
       images: [
         'https://img.wongnai.com/p/1920x0/2019/05/21/9ab9644ef6ec47b5b7d42f5acaa76b26.jpg',
         'https://pbs.twimg.com/media/DBymfTjVwAA3Zxb?format=jpg&name=medium',
         'https://img.wongnai.com/p/1920x0/2019/05/31/747be9e047b44002970857624586f781.jpg',
         'https://pbs.twimg.com/media/D5TFsKgU4AIvYxz?format=jpg&name=large',
-
-        //require('./assets/images/girl.jpg'),
       ],
     };
   }
-
+  ////
   render() {
     return (
       <View style={styles.container}>
@@ -38,18 +43,27 @@ export default class App extends Component {
         <SafeAreaView style={styles.container}>
           <ScrollView style={styles.scrollView}>
             <Text style={styles.toolbarButton}>หมวดหมู่</Text>
-            <Image
+            {/* <Image
               source={{
                 uri:
                   'https://pbs.twimg.com/media/D3isOjqV4AIkCPB?format=jpg&name=small',
                 cache: 'only-if-cached',
               }}
               style={{width: 100, height: 180}}
-            />
+            /> */}
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>ภูเขา</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>น้ำตก</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer}>
+              <Text>ทะเล</Text>
+            </TouchableOpacity>
           </ScrollView>
         </SafeAreaView>
       </View>
-        );
+    );
   }
 }
 
@@ -66,5 +80,16 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: 'white',
     marginHorizontal: 20,
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#00BFFF',
   },
 });
